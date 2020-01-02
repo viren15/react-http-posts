8716c6fc82e9c2a7e3d651f9d9e6beb156a56a01
Viren Kathiriya <virenkathiriya96@gmail.com>
2020-01-02 12:12:08 +0530

    init commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+// import axios from 'axios';
+import axios from '../../axios'
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectPostId: null,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Vir'
+                    }
+                });
+                this.setState({posts: updatedPosts });
+            })
+            .catch(error => {
+                this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something Went Wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+                }); 
+        }
+
+
+        return (
+            <div className="Blog">
+                <header >
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
